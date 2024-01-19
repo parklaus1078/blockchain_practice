74e974bd25dfd997f36012732102f9d0c1a96d04
Kay Park <parklaus1078@gmail.com>
2024-01-19 14:40:12 +0900

    blockchain example added

--- blockchain.go
+++ blockchain.go
@@ -5,12 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"strconv"
+	"strings"
 	"time"
 )
-
-func main() {
-	fmt.Println("hello, world")
-}
 
 type Block struct { // Definition of a Block
 	data         map[string]interface{} // data will be map of strings
@@ -31,4 +28,65 @@ func (b Block) calculateHash() string {
 	blockData := b.previousHash + string(data) + b.timestamp.String() + strconv.Itoa(b.pow)
 	blockHash := sha256.Sum256([]byte(blockData))
 	return fmt.Sprintf("%x", blockHash)
+}
+
+func (b *Block) mine(difficulty int) {
+	for !strings.HasPrefix(b.hash, strings.Repeat("0", difficulty)) {
+		b.pow++
+		b.hash = b.calculateHash()
+	}
+}
+
+func CreateBlockchain(difficulty int) Blockchain {
+	genesisBlock := Block{
+		hash:      "0",
+		timestamp: time.Now(),
+	}
+
+	return Blockchain{
+		genesisBlock,
+		[]Block{genesisBlock},
+		difficulty,
+	}
+}
+
+func (b *Blockchain) addBlock(from, to string, amount float64) {
+	blockData := map[string]interface{}{
+		"from":   from,
+		"to":     to,
+		"amount": amount,
+	}
+
+	lastBlock := b.chain[len(b.chain)-1]
+	newBlock := Block{
+		data:         blockData,
+		previousHash: lastBlock.hash,
+		timestamp:    time.Now(),
+	}
+
+	newBlock.mine(b.difficulty)
+	b.chain = append(b.chain, newBlock)
+}
+
+func (b Blockchain) isValid() bool { // Checking validity of all blocks in the chain
+	for i := range b.chain[1:] {
+		previousBlock := b.chain[i]
+		currentBlock := b.chain[i+1]
+		if currentBlock.hash != currentBlock.calculateHash() || currentBlock.previousHash != previousBlock.hash {
+			return false
+		}
+	}
+
+	return true
+}
+
+func main() {
+	blockchain := CreateBlockchain(2)
+
+	blockchain.addBlock("Person1", "Receiver3", 10)
+	blockchain.addBlock("Person2", "Receiver2", 23)
+	blockchain.addBlock("Receiver3", "Receiver5", 15)
+	blockchain.addBlock("Receiver2", "Receiver5", 10)
+
+	fmt.Println(blockchain.isValid())
 }
